bd1edd440d51bdd1ff7f167dbb8682133e4df0a1
Ledenel <ledenelintelli@gmail.com>
2020-11-24 19:53:00 +0800

    squash more

--- BlundellCheckstyle/src/main/java/com/blundell/checks/AntiHungarianCheck.java
+++ BlundellCheckstyle/src/main/java/com/blundell/checks/AntiHungarianCheck.java
@@ -2,9 +2,9 @@ package com.blundell.checks;
 
 import com.puppycrawl.tools.checkstyle.api.*;
 
-public class AntiHungarianCheck extends AbstractCheck {
+import java.util.regex.Pattern;
 
-    private final HungarianNotationMemberDetector detector = new HungarianNotationMemberDetector();
+public class AntiHungarianCheck extends AbstractCheck {
 
     @Override
     public int[] getDefaultTokens() {
@@ -23,7 +23,7 @@ public class AntiHungarianCheck extends AbstractCheck {
     public void visitToken(DetailAST aAST) {
         DetailAST identifier = aAST.findFirstToken(TokenTypes.IDENT);
         String variableName = identifier.toString();
-        if (aAST.getParent().getType() == TokenTypes.OBJBLOCK && detector.detectsNotation(variableName)) {
+        if (aAST.getParent().getType() == TokenTypes.OBJBLOCK && Pattern.compile("m[A-Z0-9].*").matcher(variableName).matches()) {
             this.log(aAST.getLineNo(), ("Hungarian notation belongs in the 90's. " +
                     "Don't prefix member variables with 'm'. " +
                     "Use your IDE's shiny colors. Culprit was: ") + variableName);
