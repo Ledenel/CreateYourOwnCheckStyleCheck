f3d11fcb322e72d753fe00026d0b5c8b81e33da2
Ledenel <ledenelintelli@gmail.com>
2020-11-24 21:03:45 +0800

    add init for AST_ID_MAP

--- BlundellCheckstyle/src/main/java/cn/beihangsoft/checks/DeckardCheck.java
+++ BlundellCheckstyle/src/main/java/cn/beihangsoft/checks/DeckardCheck.java
@@ -1,7 +1,14 @@
 package cn.beihangsoft.checks;
 
 import com.puppycrawl.tools.checkstyle.api.AbstractCheck;
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
 import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.stream.Collectors;
 
 public class DeckardCheck extends AbstractCheck {
     @Override
@@ -19,5 +26,28 @@ public class DeckardCheck extends AbstractCheck {
         return getAcceptableTokens();
     }
 
+    private HashMap<String, Integer> AST_ID_MAP;
 
+    @Override
+    public void init() {
+        AST_ID_MAP = new HashMap<>();
+        for (Field field:
+             Arrays.stream(TokenTypes.class.getDeclaredFields())
+                .filter(field -> {
+                    int modifiers = field.getModifiers();
+                    return Modifier.isPublic(modifiers) && Modifier.isStatic(modifiers) && Modifier.isFinal(modifiers) && field.getType().isAssignableFrom(int.class);
+                }).collect(Collectors.toList())) {
+            try {
+                AST_ID_MAP.put(field.getName(), field.getInt(null));
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(AST_ID_MAP);
+    }
+
+    @Override
+    public void visitToken(DetailAST ast) {
+
+    }
 }
