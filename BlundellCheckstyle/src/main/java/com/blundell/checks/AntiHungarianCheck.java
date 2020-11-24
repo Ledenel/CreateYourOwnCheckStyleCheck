92afce24092d3c0e04e923b52f9171bf23275c60
Ledenel <ledenelintelli@gmail.com>
2020-11-24 19:43:17 +0800

    add DeckardCheck, squash micro design in check

--- BlundellCheckstyle/src/main/java/com/blundell/checks/AntiHungarianCheck.java
+++ BlundellCheckstyle/src/main/java/com/blundell/checks/AntiHungarianCheck.java
@@ -4,10 +4,6 @@ import com.puppycrawl.tools.checkstyle.api.*;
 
 public class AntiHungarianCheck extends AbstractCheck {
 
-    private static final String CATCH_MSG = "Hungarian notation belongs in the 90's. " +
-    		                                "Don't prefix member variables with 'm'. " +
-    		                                "Use your IDE's shiny colors. Culprit was: ";
-
     private final HungarianNotationMemberDetector detector = new HungarianNotationMemberDetector();
 
     @Override
@@ -25,23 +21,13 @@ public class AntiHungarianCheck extends AbstractCheck {
 
     @Override
     public void visitToken(DetailAST aAST) {
-        String variableName = findVariableName(aAST);
-        if (itsAFieldVariable(aAST) && detector.detectsNotation(variableName)) {
-            reportStyleError(aAST, variableName);
-        }
-    }
-
-    private String findVariableName(DetailAST aAST) {
         DetailAST identifier = aAST.findFirstToken(TokenTypes.IDENT);
-        return identifier.toString();
-    }
-
-    private boolean itsAFieldVariable(DetailAST aAST) {
-        return aAST.getParent().getType() == TokenTypes.OBJBLOCK;
-    }
-
-    private void reportStyleError(DetailAST aAST, String variableName) {
-        log(aAST.getLineNo(), CATCH_MSG + variableName);
+        String variableName = identifier.toString();
+        if (aAST.getParent().getType() == TokenTypes.OBJBLOCK && detector.detectsNotation(variableName)) {
+            this.log(aAST.getLineNo(), ("Hungarian notation belongs in the 90's. " +
+                    "Don't prefix member variables with 'm'. " +
+                    "Use your IDE's shiny colors. Culprit was: ") + variableName);
+        }
     }
 
 }
